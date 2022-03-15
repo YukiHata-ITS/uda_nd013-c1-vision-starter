19e8e613d6f59a97c331fe233cc05b9a810fdc97
Yuki hata <72856244+YukiHata-ITS@users.noreply.github.com>
2022-03-15 11:41:09 +0900

    project instructions 1
    Exploratory Data Analysis, Create the splits and Explore augmentations

--- workspace/create_splits.py
+++ workspace/create_splits.py
@@ -7,6 +7,8 @@ import numpy as np
 
 from utils import get_module_logger
 
+from random import shuffle      # No2 分割関数の作成
+import shutil                   # No2 分割関数の作成
 
 def split(data_dir):
     """
@@ -20,6 +22,51 @@ def split(data_dir):
     # TODO: Split the data present in `/home/workspace/data/waymo/training_and_validation` into train and val sets.
     # You should move the files rather than copy because of space limitations in the workspace.
 
+    ##### ファイルディレクトリ名取得
+    ##### waymoフォルダ
+    basefolder_dir = data_dir
+    extension = 'tfrecord'
+    basefile_dir = os.path.join(basefolder_dir, 'waymo', 'training_and_validation' , '*.' + extension)
+    print('basefile_dir', basefile_dir)
+
+    ##### 分割先ディレクトリ名設定
+    train_dir = os.path.join(basefolder_dir, 'train')
+    val_dir   = os.path.join(basefolder_dir, 'val')
+    test_dir  = os.path.join(basefolder_dir, 'test')
+    print('train_dir', train_dir)
+    
+    ##### 分割先ディレクトリ作成    
+    os.makedirs(train_dir, exist_ok=True)
+    os.makedirs(val_dir, exist_ok=True)
+    os.makedirs(test_dir, exist_ok=True)
+    
+    ##### ファイル配分割合 ['train', 'val', 'test']
+    dir_ratio = [0.7, 0.2, 0.1]
+        
+    ##### ファイル群取得　glob.globを使う。
+    files = glob.glob(os.path.join(basefile_dir))
+    print('files', files)
+    
+    ##### シャッフル
+    shuffle(files)
+    
+    ##### ファイル数算出
+    total_files = len(files)
+    num_train = int(dir_ratio[0]/sum(dir_ratio) * total_files)
+    num_val = int(dir_ratio[1]/sum(dir_ratio) * total_files)
+    print('total_files', total_files)
+    print('num_train', num_train)
+    print('num_val', num_val)
+    
+    ##### ファイル移動
+    for file in files[:num_train]:
+        new_path = shutil.move(file, train_dir)
+    for file in files[num_train:num_train+num_val]:
+        new_path = shutil.move(file, val_dir)
+    for file in files[num_train+num_val:]:
+        new_path = shutil.move(file, test_dir)
+        
+    
 if __name__ == "__main__": 
     parser = argparse.ArgumentParser(description='Split data into training / validation / testing')
     parser.add_argument('--data_dir', required=True,
